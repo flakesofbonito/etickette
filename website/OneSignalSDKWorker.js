8c4802c0c7322477faa0d5e55f87a071da9783aa
Kurt Luis Grape <kurtluisgrape.gmail.com>
2026-04-20 19:08:02 +0800

    Brah

--- website/OneSignalSDKWorker.js
+++ website/OneSignalSDKWorker.js
@@ -1,1 +1,7 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+try {
+    importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+} catch(e) {
+    console.warn('[OneSignal SW] Failed to load:', e);
+}
